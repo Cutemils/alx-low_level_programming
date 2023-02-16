9a31c0f662747d11b49c7b8a2212e1afa2c533f1
cutemils <mmilicentazomokwu@yahoo.com>
2023-02-16 09:45:38 -0800

    task 6 commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,10 +13,10 @@ int main(void)
 	long long int p;
 	float q;
 
-printf("Size of a char; %lu byte(s)\n", (unsigned long)sizeof(m));
-printf("size of an int; %lu byte(s)\n", (unsigned long)sizeof(n));
-printf("size of a long int %lu byte(s)\n", (unsigned long)sizeof(0));
-printf("size of a long long int %lu byte(s)\n", (unsigned long)sizeof(p));
-printf("size of a float %lu byte(s)\n", (unsigned long)sizeof(q));
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(m));
+printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(n));
+printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(0));
+printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(p));
+printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(q));
 return (0);
 }
