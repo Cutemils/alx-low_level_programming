8a19381f1735fc6bebc6ef61d924770b08871796
cutemils <mmilicentazomokwu@yahoo.com>
2023-02-16 09:41:58 -0800

    task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - aA program that writes various computer types
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char m;
+	int n;
+	long int o;
+	long long int p;
+	float q;
+
+printf("Size of a char; %lu byte(s)\n", (unsigned long)sizeof(m));
+printf("size of an int; %lu byte(s)\n", (unsigned long)sizeof(n));
+printf("size of a long int %lu byte(s)\n", (unsigned long)sizeof(0));
+printf("size of a long long int %lu byte(s)\n", (unsigned long)sizeof(p));
+printf("size of a float %lu byte(s)\n", (unsigned long)sizeof(q));
+return (0);
+}
