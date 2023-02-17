af19888c49bc1ab3b1e4bd35d04847cf0cbd0af4
cutemils <mmilicentazomokwu@yahoo.com>
2023-02-17 06:00:02 -0800

    commit 3

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Print lower case
+ * Return: 0
+ */
+int main(void)
+{
+	char tj;
+
+	for (tj = 'a'; tj <= 'z'; tj++)
+	{
+		putchar(tj);
+	}
+	putchar('\n');
+	return (0);
+}
