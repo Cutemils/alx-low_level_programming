206d544718abea29c1b0e30131bb83cf07fa526a
cutemils <mmilicentazomokwu@yahoo.com>
2023-02-16 20:35:59 -0800

    commit task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - A program that writes various computer types
- * Return: Always 0 (Success)
+ * Return: 0 (Success)
  */
 int main(void)
 {
@@ -9,12 +9,12 @@ int main(void)
 	int n;
 	long int o;
 	long long int p;
-	float q;
+	float f;
 
 printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(m));
 printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(n));
 printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(0));
 printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(p));
-printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(q));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
