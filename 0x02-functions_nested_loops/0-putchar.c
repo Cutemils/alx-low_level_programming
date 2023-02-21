9c3a0d1cadf031a03485aaae486adf0939f4780b
cutemils <mmilicentazomokwu@yahoo.com>
2023-02-21 15:01:36 -0800

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - putchar
+ * Return: 0
+ */
+
+int main(void)
+{
+	_putchar("_");
+	_putchar("p");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("Y");
+	_putchar("\n");
+	return (0);
+}
