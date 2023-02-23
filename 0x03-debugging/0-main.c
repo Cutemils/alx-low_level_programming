c58a81266bb2971266ed0ebf313256b270ea1b08
cutemils <mmilicentazomokwu@yahoo.com>
2023-02-23 09:51:40 -0800

    commit

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - positive or negative
+ * Return: Always 0
+ */
+int main(void)
+{
+       int n;	
+        if (n > 0)
+        {
+                printf("%d is positive\n", n);
+        }
+        else if (n == 0)
+        {
+                printf("%d is zero\n", n);
+        }
+        else
+        {
+        printf("%d is negative\n", n);
+}
+        return (0);
+}
