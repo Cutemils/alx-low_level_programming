6ad4fb5c0b276359573ac62abfd4aed116580901
cutemils <mmilicentazomokwu@yahoo.com>
2023-02-22 20:18:06 -0800

    commit correct

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,20 @@
 #include "main.h"
 /**
- * main - putchar
- * Return: 0
+ * main - Entry point
+ *
+ * Return: Always returns 0
+ *
  */
-
 int main(void)
 {
-	_putchar("_");
-	_putchar("p");
-	_putchar("h");
-	_putchar("a");
-	_putchar("Y");
-	_putchar("\n");
-	return (0);
+        putchar('');
+        _putchar('p');
+        _putchar('u');
+        _putchar('t');
+        _putchar('c');
+        _putchar('h');
+        _putchar('a');
+        _putchar('r');
+        _putchar('\n');
+        return (0);
 }
